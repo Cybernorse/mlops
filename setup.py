08e21ab6355d2c1e034ed61ba3d4948ca4267696
WaqarAhmad <waqarsher66@gmail.com>
2023-02-08 02:44:29 +0500

    tox and stuff

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mlops",
+    version="0.0.1",
+    description="its a mlops piepeline development tools test",
+    author="bigpenguin",
+    package=find_packages(),
+    license="MIT"
+)
